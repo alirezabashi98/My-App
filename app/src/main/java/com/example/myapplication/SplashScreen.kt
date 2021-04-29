4c9427b685613899a389668f9e31942187f9cfad
alirezabashi98 <alirezabashi98@gmail.com>
2021-04-29 12:01:32 +0430

    demo view splash & add library retrofit v 2.9.0

--- app/src/main/java/com/example/myapplication/SplashScreen.kt
+++ app/src/main/java/com/example/myapplication/SplashScreen.kt
@@ -2,10 +2,18 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.wang.avi.AVLoadingIndicatorView
 
 class SplashScreen : AppCompatActivity() {
+
+    lateinit var avi : AVLoadingIndicatorView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        avi = findViewById(R.id.avi)
+        avi.show()
+
     }
 }
