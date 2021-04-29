211421aead6b470619efa1e55dd1d10d5d9bbcd5
alirezabashi98 <alirezabashi98@gmail.com>
2021-04-29 12:13:46 +0430

    end demo SplashScreen

--- app/src/main/java/com/example/myapplication/SplashScreen.kt
+++ app/src/main/java/com/example/myapplication/SplashScreen.kt
@@ -2,18 +2,26 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.wang.avi.AVLoadingIndicatorView
 
 class SplashScreen : AppCompatActivity() {
 
     lateinit var avi : AVLoadingIndicatorView
+    lateinit var message : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        avi = findViewById(R.id.avi)
-        avi.show()
+        Cast()
+
+    }
+
+    fun Cast(){
+
+        avi = findViewById(R.id.avi_SplashScreen_loading)
+        message = findViewById(R.id.tv_SplashScreen_message)
 
     }
 }
